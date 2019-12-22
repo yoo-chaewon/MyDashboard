ea407f66075f96f870607ea9d16735acb9783804
유채원 <ohwow5@sookmyung.ac.kr>
2019-12-22 16:07:22 +0900

    MAKE Test project

---
+++ STUDY/TestProject/index.js
@@ -0,0 +1,15 @@
+const title = document.querySelector("#title");
+const CLICKED_CLASS = "clicked";
+
+function handleClick(){
+    //toggle: 클래스가 있는지 체크해서 없으면 add 있으면 remove해주는 함수
+    title.classList.toggle(CLICKED_CLASS);
+}
+
+
+function init(){
+    title.addEventListener("click", handleClick);
+}
+
+init();
+
